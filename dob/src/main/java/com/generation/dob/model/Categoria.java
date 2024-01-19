0fb1061db14b3e41989ca579c39ee6a9bff11a32
Verônica Freitas Santos <verofreit@gmail.com>
2024-01-19 11:50:42 -0300

    criado model usuario, model produto, relacoes entre tabelas e criado crud de produto

--- dob/src/main/java/com/generation/dob/model/Categoria.java
+++ dob/src/main/java/com/generation/dob/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.generation.dob.model;
 
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -20,7 +27,18 @@ public class Categoria {
 	@Size(min = 5, max = 255, message = "Este atributo deve conter no mínimo 5 caracteres e no máximo 255")
 	private String tipo;
 	
-	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 	public Long getId() {
 		return id;
 	}
